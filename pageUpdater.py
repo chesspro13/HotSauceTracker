5f16214d42bd2e6b17b22d0c778f4ad4022189e8
chesspro13 <chesspro13@gmail.com>
2020-05-03 09:36:21 -0600

    Initial commit

--- pageUpdater.py
+++ pageUpdater.py
@@ -49,6 +49,37 @@ def fileCreator( Name, Company, Cost, Stars, Ingredients, ImgURL, URL ):
 			</tr>
 		</table>
 		<a href="''' + URL + '''"><input type=button value='HOME'></a>
+		<script >
+			
+			function output( output )
+			{
+				document.getElementById("output").innerHTML = output;
+			}
+			function updateFont()
+			{
+				let text = document.getElementById("site_name");
+				let width = window.innerWidth;
+				if( width >= 1500 )
+					text.style.fontSize = "80px";
+				else if( width >=1350 && width < 1500 )
+					text.style.fontSize = "64px";
+				else if( width >=1200 && width < 1350 )
+					text.style.fontSize = "55px";
+				else if( width >=1050 && width < 1200 )
+					text.style.fontSize = "46px";
+				else if( width >=900 && width < 1050 )
+					text.style.fontSize = "38px";
+				else if( width >=750 && width < 900 )
+					text.style.fontSize = "30px";
+				else if( width >=600 && width < 750 )
+					text.style.fontSize = "26px";
+				else 
+					text.style.fontSize = "22px";
+				output( window.innerWidth );
+			}
+			output( window.innerWidth );
+			updateFont();
+		</script>
 	</body>
 </html>'''
 
@@ -79,34 +110,36 @@ def getInfo():
 			
 	if debug:
 		print( "[" + loginInfo['password'] + ']' )
+		
+	return loginInfo
  
 try:
-    login = getInfo()
-    connection = mysql.connector.connect( login['host'], login['user'], login['password'], login['db'] )
+	login = getInfo()
+	print( login )
+	connection = mysql.connector.connect( host='localhost', user=login['username'], password=login['password'], db=login['db'] )
+	cur = connection.cursor()
+	cur.execute("SELECT * FROM sauces")
 
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM sauces")
+	rows = cur.fetchall()
+	   #db_info = connection.get_server_info()
+	#print( "Connected to MySQL server version ", db_info )
+	#cursor = connection.cursor()
+	#cursor.execute("select database();")
+	#record = cursor.fetchone()
+	#print("You're connected to database: ", record )
 
-    rows = cur.fetchall()
-           #db_info = connection.get_server_info()
-        #print( "Connected to MySQL server version ", db_info )
-        #cursor = connection.cursor()
-        #cursor.execute("select database();")
-        #record = cursor.fetchone()
-        #print("You're connected to database: ", record )
+	#cursor.execute("SELECT * FROM sauces")
+	#result = cursor.fetchall()
 
-        #cursor.execute("SELECT * FROM sauces")
-        #result = cursor.fetchall()
-
-    for sauce in rows:
-#        print( sauce[0] )
-#        print( sauce[1] )
-#        print( sauce[2] )
-#        print( sauce[3] )
-#        print( sauce[4] )
-#        print( sauce[5] )
-#        print( sauce[6] )
-        fileCreator( sauce[0], sauce[1], sauce[2], sauce[4], sauce[3], sauce[6], sauce[5] )
+	for sauce in rows:
+	#        print( sauce[0] )
+	#        print( sauce[1] )
+	#        print( sauce[2] )
+	#        print( sauce[3] )
+	#        print( sauce[4] )
+	#        print( sauce[5] )
+	#        print( sauce[6] )
+		fileCreator( sauce[0], sauce[1], sauce[2], sauce[4], sauce[3], sauce[6], sauce[5] )
         
 
 except Error as e:
