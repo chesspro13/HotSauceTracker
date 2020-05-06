a9de41508ab2bb4103574e4f19650bf861f3180d
chesspro13 <chesspro13@gmail.com>
2020-05-06 15:31:15 -0600

    Started header for new design

---
+++ newHeader.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+	<header>
+	<link rel="stylesheet" href="css/headerStyle.css">
+		<img class="header_image" src="images/banner.png" alt="Header Image">
+		<h1 class="header_text">The Mauldins Hot-Net</h1>
+		<div class="menu_container">
+			<div class= "menu_button_container" >
+				<h3 class="menu_text">Home</h3>
+			</div>
+			<div class= "menu_button_container" >
+				<h3 class="menu_text">Gallery</h3>
+			</div>
+			<div class= "menu_button_container" >
+				<h3 class="menu_text">Add Sauce</h3>
+			</div>
+			<div class= "menu_button_container" >
+				<h3 class="menu_text">Edit Sauce</h3>
+			</div>
+			<div class= "menu_button_container" >
+				<h3 class="menu_text">Recomendation</h3>
+			</div>
+		</div>
+		<div class="banner_border_block">
+			<img class="banner_image" src="images/banner.jpg" alt="Banner Image">
+			<h2 class="banner_text_top">Sauces for every occasion</h2>
+			<h2 class="banner_text_bottom">For that wonderful burning sensation</h2>
+		</div>
+	</header>
+</html>
